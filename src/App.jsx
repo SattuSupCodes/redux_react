ff811a4760c1984189b4ac165a76a5f4fddc4ac8
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-12-03 13:00:00 +0530

    effect component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import CounterComponent from './components/CounterComponent'
 import EffectComponent from './components/Effect'
 function App() {
   const [count, setCount] = useState(0)
+  const[isVisible, setVisible] = useState(true)
 
   return (
     <>
+
     <CounterComponent></CounterComponent>
-    <EffectComponent></EffectComponent>
+    {/* <EffectComponent></EffectComponent> */}
+    {isVisible?<EffectComponent/> :<></>}
+    <button onClick={() => setVisible(!isVisible)}>Toggle</button>
     </>
   )
 }
