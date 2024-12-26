5e0dff45737b75169ea6c0201b23d356bd6d5180
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-12-27 02:30:30 +0530

    counter component

---
+++ src/components/CounterComponent.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const CounterComponent = () => {
+    const [count, setCount] = useState(12);
+    const [value, setValue] = useState("number");
+
+    return (
+        <div>
+           <p>
+            Count Component - {count}
+           </p>
+           <button onClick={() => setCount(count + 1)}>increment</button>
+           <button onClick={() => setCount(count - 1)}>decrement</button>
+           <button onClick={()=> setValue(count%2==0 ? "True" : "False")}>odd/even</button>
+           
+        </div>
+    );
+};
+
+export default CounterComponent;
